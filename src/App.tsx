ea568f45582087f4adbfda483ff660861bc415aa
Md. Numanur Rahman <2001011@iot.bdu.ac.bd>
2025-02-12 15:45:54 +0600

    Added Buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import Fun from "./components/Fun";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <Fun />
+      <h1>Dear Hayatim, Will you be my valentine?</h1>
+      <div>
+        <button>Yes</button>
+        <button>No</button>
+      </div>
+      <div className="gif_container">
+        <img
+          src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbW5lenZyZHI5OXM2eW95b3pmMG40cWVrMDhtNjVuM3A4dGNxa2g2dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/VM1fcpu2bKs1e2Kdbj/giphy.gif"
+          alt="Cute GIF"
+        />
+      </div>
     </>
   );
 }
