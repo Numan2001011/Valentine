3b720941103af26374e7be3e710c7b24fcc1eae4
Md. Numanur Rahman <2001011@iot.bdu.ac.bd>
2025-02-12 16:17:16 +0600

    Added Buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
+import { useState } from "react";
 import "./App.css";
+import Mainpage from "./components/Mainpage";
 
 function App() {
+  const [component, showComponent] = useState(false);
+  const onStart = () => {
+    showComponent(true);
+  };
   return (
-    <>
-      <h1>Dear Hayatim, Will you be my valentine?</h1>
-      <div>
-        <button>Yes</button>
-        <button>No</button>
-      </div>
-      <div className="gif_container">
-        <img
-          src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbW5lenZyZHI5OXM2eW95b3pmMG40cWVrMDhtNjVuM3A4dGNxa2g2dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/VM1fcpu2bKs1e2Kdbj/giphy.gif"
-          alt="Cute GIF"
-        />
-      </div>
-    </>
+    <>{!component ? <button onClick={onStart}>START</button> : <Mainpage />}</>
   );
 }
 
---
+++ src/components/Yes.tsx
@@ -0,0 +1,13 @@
+import "./Yes.css";
+const Yes = () => {
+  return (
+    <div className="container">
+      <h1 className="header_text">Knew you would say yes!</h1>
+      <div className="gif_container_2">
+        <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmo3c3l5ODh3ZGN6NHhhaDE2Mjg1ZjkwOXczdDFxbWM3dTBtaW9zaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/9XY4f3FgFTT4QlaYqa/giphy.gif" />
+      </div>
+    </div>
+  );
+};
+
+export default Yes;
